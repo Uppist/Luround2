688d5823ba8a952ff49c1a192cbf180482f12443
seniorman-dev <coder.jay2001@gmail.com>
2023-10-28 21:41:19 +0100

    another hit

---
+++ lib/controllers/account_owner/bookins_controller.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart' as getx;
+
+
+
+
+
+
+
+
+
+
+class BookingsController extends getx.GetxController {}

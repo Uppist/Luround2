ecd340e32cea8942bd4497ee3913a44e44699dc7
seniorman-dev <coder.jay2001@gmail.com>
2023-11-08 13:23:40 +0100

    update

---
+++ lib/controllers/account_owner/authentication_controller.dart
@@ -0,0 +1,12 @@
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart' as getx;
+
+
+
+
+
+
+class AuthController extends getx.GetxController {}

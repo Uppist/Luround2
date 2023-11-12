a2a6de89c8c9bb70fb28421dce95f88458d70df1
seniorman-dev <coder.jay2001@gmail.com>
2023-11-13 00:27:13 +0100

    okay

--- lib/main.dart
+++ lib/main.dart
@@ -50,11 +50,10 @@ class _MainAppState extends State<MainApp> {
       child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Luround',
-        home: MainPage(), //MainPageAccViewer(), //MainPage(), //SplashScreen1()
+        home: SplashScreen1(), //MainPageAccViewer(), //MainPage(), //SplashScreen1()
         supportedLocales: [
-          Locale('en'), // English
+          Locale('en'), // English Locale
         ]
-        //home: SplashScreen(),
       ),
     );
   }

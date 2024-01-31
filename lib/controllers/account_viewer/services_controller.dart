3ed1f7a9272971bad4907d7993680718f8543931
seniorman-dev <coder.jay2001@gmail.com>
2024-01-31 18:39:45 +0100

    modified:   lib/controllers/account_viewer/services_controller.dart 	modified:   lib/main.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/controllers/account_viewer/services_controller.dart
+++ lib/controllers/account_viewer/services_controller.dart
@@ -7,7 +7,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart' as getx;
 import 'package:luround/utils/colors/app_theme.dart';
 import 'package:luround/utils/components/my_snackbar.dart';
-import 'package:image_picker_web/image_picker_web.dart';
+//import 'package:image_picker_web/image_picker_web.dart';
 
 
 
@@ -73,7 +73,7 @@ class AccViewerServicesController extends getx.GetxController {
   //pick image from gallery, display the image picked and upload to cloudinary sharps.
   Future<void> pickFileForPayment(BuildContext context) async {
     
-    try {
+    /*try {
       final pickedImage = await ImagePickerWeb.getImageAsBytes(); 
       if (pickedImage != null) {
         imageFromGallery.value = pickedImage;
@@ -91,7 +91,7 @@ class AccViewerServicesController extends getx.GetxController {
         backgroundColor: AppColor.redColor,
         message: "Error picking image: $e"
       );
-    }
+    }*/
 
   }
 
@@ -107,7 +107,7 @@ class AccViewerServicesController extends getx.GetxController {
   //pick image from gallery, display the image picked and upload to cloudinary sharps.
   Future<void> selectFile(BuildContext context) async {
     
-    try {
+    /*try {
       final pickedImage = await ImagePickerWeb.getImageAsBytes(); //.pickImage(source: ImageSource.gallery);
       if (pickedImage != null) {
         selectedFileForRequestingQuote.value = pickedImage;
@@ -125,7 +125,7 @@ class AccViewerServicesController extends getx.GetxController {
         backgroundColor: AppColor.redColor,
         message: "Error picking image: $e"
       );
-    }
+    }*/
   }
 
 

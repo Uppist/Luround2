26306b1816a08c37dc596d0342e1e12bff66c2d7
seniorman-dev <coder.jay2001@gmail.com>
2024-02-29 09:35:25 +0100

    modified:   android/build.gradle 	modified:   lib/controllers/account_owner/main/mainpage_controller.dart 	modified:   lib/views/account_owner/payment_screen/screen/payment_screen.dart 	modified:   windows/flutter/CMakeLists.txt 	modified:   windows/runner/flutter_window.cpp

--- lib/views/account_owner/payment_screen/screen/payment_screen.dart
+++ lib/views/account_owner/payment_screen/screen/payment_screen.dart
@@ -1,14 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:luround/utils/colors/app_theme.dart';
 
 
 
-class SubscriptionScreen
- extends StatelessWidget {
+class SubscriptionScreen extends StatefulWidget {
   const SubscriptionScreen
   ({super.key});
 
   @override
+  State<SubscriptionScreen> createState() => _SubscriptionScreenState();
+}
+
+class _SubscriptionScreenState extends State<SubscriptionScreen> with SingleTickerProviderStateMixin {
+  
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColor.mainColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 40.h,),
+          Text(
+            "Pricing Plan",
+            style: GoogleFonts.inter(
+              color: AppColor.bgColor,
+              fontSize: 24.sp,
+              fontWeight: FontWeight.w600
+            ),
+          ),
+        ],
+      )
+    );
   }
 }

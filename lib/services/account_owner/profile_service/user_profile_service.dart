ef27d217e38e5e3ec759a35aeb4d1337e6fe5f72
seniorman-dev <coder.jay2001@gmail.com>
2024-03-08 17:39:51 +0100

    modified:   lib/main.dart 	modified:   lib/services/account_owner/profile_service/user_profile_service.dart

--- lib/services/account_owner/profile_service/user_profile_service.dart
+++ lib/services/account_owner/profile_service/user_profile_service.dart
@@ -40,6 +40,7 @@ class AccOwnerProfileService extends getx.GetxController {
   
   final isLoading = false.obs;
   var isBannerCancelled = false.obs;
+  
   var userId = LocalStorage.getUserID();
   var userEmail = LocalStorage.getUseremail();
   var token = LocalStorage.getToken();

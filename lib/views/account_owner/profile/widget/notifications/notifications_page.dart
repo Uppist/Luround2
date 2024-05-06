e0b77fd8baed9abb35a0228c2b12599ba5156923
seniorman-dev <coder.jay2001@gmail.com>
2024-05-06 23:43:19 +0100

    modified:   lib/views/account_owner/profile/widget/notifications/notifications_page.dart

--- lib/views/account_owner/profile/widget/notifications/notifications_page.dart
+++ lib/views/account_owner/profile/widget/notifications/notifications_page.dart
@@ -106,96 +106,98 @@ class NotificationsPage extends StatelessWidget {
                     ),
                   ),
                   ////
-                  sortedList.isNotEmpty ? ListView.separated(
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    physics: BouncingScrollPhysics(),
-                    itemCount: sortedList.length,
-                    separatorBuilder: (context, index) => Divider(color: AppColor.darkGreyColor, thickness: 0.2,),
-                    itemBuilder: (context, index) { 
-                  
-                      final item = sortedList[index];
-                      
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
+                  sortedList.isNotEmpty ? Expanded(
+                    child: ListView.separated(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: sortedList.length,
+                      separatorBuilder: (context, index) => Divider(color: AppColor.darkGreyColor, thickness: 0.2,),
+                      itemBuilder: (context, index) { 
                     
-                          SizedBox(height: 10.h),
-                                      
-                          /*Container(
-                            color: AppColor.greyColor,
-                            width: double.infinity,
-                            height: 7,
-                          ),*/
-                          
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              color: AppColor.bgColor,
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                CircleAvatar(
-                                  backgroundColor: AppColor.mainColor,
-                                  radius: 30.r,
-                                  child: Text(
-                                    item['title'].toString().toUpperCase().substring(0, 1),
-                                    style: GoogleFonts.inter(
-                                      color: AppColor.bgColor,
-                                      fontSize: 16.sp,
-                                      fontWeight: FontWeight.w600
+                        final item = sortedList[index];
+                        
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                      
+                            SizedBox(height: 10.h),
+                                        
+                            /*Container(
+                              color: AppColor.greyColor,
+                              width: double.infinity,
+                              height: 7,
+                            ),*/
+                            
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: AppColor.bgColor,
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CircleAvatar(
+                                    backgroundColor: AppColor.mainColor,
+                                    radius: 30.r,
+                                    child: Text(
+                                      item['title'].toString().toUpperCase().substring(0, 1),
+                                      style: GoogleFonts.inter(
+                                        color: AppColor.bgColor,
+                                        fontSize: 16.sp,
+                                        fontWeight: FontWeight.w600
+                                      ),
                                     ),
                                   ),
-                                ),
-                  
-                                SizedBox(width: 15.w),
-                                
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        item['title'],  //recepient name
-                                        style: GoogleFonts.inter(
-                                          textStyle: TextStyle(
-                                            overflow: TextOverflow.visible,
-                                            color: AppColor.blackColor,
-                                              fontWeight: FontWeight.w600,
-                                              fontSize: 14.sp,
+                    
+                                  SizedBox(width: 15.w),
+                                  
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          item['title'],  //recepient name
+                                          style: GoogleFonts.inter(
+                                            textStyle: TextStyle(
+                                              overflow: TextOverflow.visible,
+                                              color: AppColor.blackColor,
+                                                fontWeight: FontWeight.w600,
+                                                fontSize: 14.sp,
+                                            )
                                           )
-                                        )
-                                      ),
-                                      SizedBox(height: 5.h,),
-                                      Text(
-                                        item['body'],
-                                        style: GoogleFonts.inter(
-                                          color: AppColor.darkGreyColor,
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 14.sp
-                                        )
-                                      ),
-                                      SizedBox(height: 5.h,),
-                                      Text(
-                                        convertServerTimeToDate(item['created_at'] ?? 0) ,
-                                        style: GoogleFonts.inter(
-                                          color: AppColor.darkGreyColor.withOpacity(0.5),
-                                          fontWeight: FontWeight.w400,
-                                          fontSize: 13.sp
-                                        )
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ]
-                            )
-                          ),
-                        ],
-                      );
-                    }
-                    ): NotificationEmptyState(
+                                        ),
+                                        SizedBox(height: 5.h,),
+                                        Text(
+                                          item['body'],
+                                          style: GoogleFonts.inter(
+                                            color: AppColor.darkGreyColor,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 14.sp
+                                          )
+                                        ),
+                                        SizedBox(height: 5.h,),
+                                        Text(
+                                          convertServerTimeToDate(item['created_at'] ?? 0) ,
+                                          style: GoogleFonts.inter(
+                                            color: AppColor.darkGreyColor.withOpacity(0.5),
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 13.sp
+                                          )
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ]
+                              )
+                            ),
+                          ],
+                        );
+                      }
+                      ),
+                  ): NotificationEmptyState(
                       onPressed: () {
                         userProfileService.getUserNotifications();
                       }
